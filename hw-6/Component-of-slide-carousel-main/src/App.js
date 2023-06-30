3975fc13199a25a61d15099248d9731573353bd1
voxid02 <129301401+voxid02@users.noreply.github.com>
2023-07-01 02:08:25 +0300

    Add files via upload

---
+++ hw-6/Component-of-slide-carousel-main/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Slider from "./component/Slider"
+
+function App() {
+  return (
+    <div>
+      <Slider />
+    </div>
+  );
+}
+
+export default App;
---
+++ hw-6/Component-of-slide-carousel-main/src/data.js
@@ -0,0 +1,43 @@
+const people =[
+  {
+  id: 1,
+  image:"/images/1jpg.jpg",
+  name: "Arianna",
+  title: "Singer",
+  quote:
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, aut aliquid nam minima itaque voluptates culpa corrupti error voluptatibus placeat quisquam atque ipsum, optio nobis ipsam explicabo facilis quae esse."
+},
+{
+  id: 2,
+  image:"/images/2jpg.jpg",
+  name: "Alba",
+  title: "actress",
+  quote:
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, aut aliquid nam minima itaque voluptates culpa corrupti error voluptatibus placeat quisquam atque ipsum, optio nobis ipsam explicabo facilis quae esse."
+},
+{
+  id: 3,
+  image:"/images/3jpg.jpg",
+  name: "Monika",
+  title: "Blogger",
+  quote:
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, aut aliquid nam minima itaque voluptates culpa corrupti error voluptatibus placeat quisquam atque ipsum, optio nobis ipsam explicabo facilis quae esse."
+},
+{
+  id: 4,
+  image:"/images/4jpg.jpg",
+  name: "Cristy",
+  title: "Manager",
+  quote:
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, aut aliquid nam minima itaque voluptates culpa corrupti error voluptatibus placeat quisquam atque ipsum, optio nobis ipsam explicabo facilis quae esse."
+},
+{
+  id: 5,
+  image:"/images/5jpg.jpg",
+  name: "Adele",
+  title: "Influencer",
+  quote:
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, aut aliquid nam minima itaque voluptates culpa corrupti error voluptatibus placeat quisquam atque ipsum, optio nobis ipsam explicabo facilis quae esse."
+},
+]
+export default people
